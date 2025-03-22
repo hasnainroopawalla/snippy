3cae2b9b77d7c3032bd705050a1d43c970c08831
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2025-03-22 22:22:03 +0100

    WIP

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,10 +8,14 @@ export const Navbar: React.FC = () => {
   const onClickNew = React.useCallback(() => navigate({ to: "/" }), [navigate]);
 
   return (
-    <div className="flex flex-row flex-wrap gap-6 text-lg">
-      <span className="font-mono text-primary-text">✂️ Snippy</span>
-      <TextAnchor label="New" onClick={onClickNew} />
-      <TextAnchor label="Delete" onClick={() => {}} />
+    <div className="flex flex-row flex-wrap items-center justify-between gap-4">
+      <div className="flex flex-row flex-wrap gap-6 text-lg">
+        <span className="font-mono text-primary-text">✂️ Snippy</span>
+        <TextAnchor label="New" onClick={onClickNew} />
+        <TextAnchor label="Find" onClick={() => {}} />
+        <TextAnchor label="Help" onClick={() => {}} />
+      </div>
+      {/* <SunIcon className="text-primary-text" /> */}
     </div>
   );
 };
