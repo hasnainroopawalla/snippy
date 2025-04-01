d014b66687cb7f5df2fe60d280d2681f7b0cd4d3
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2025-04-01 08:08:54 +0200

    remove config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-console.log("YAAAA", process.env.NODE_ENV);
+
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
